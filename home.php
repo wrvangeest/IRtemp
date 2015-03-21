ecefa3ee7fc0b9e2bc2fb40af9decab4777a29ab
W.R. van Geest <wiebevangeest@hotmail.com>
2015-03-21 21:07:29 +0100

    Somewhat working popup

--- home.php
+++ home.php
@@ -1,19 +1,130 @@
 <?php 
-$curl = curl_init();
-curl_setopt_array($curl, array(
-	//I want to get this one to work:
-	CURLOPT_URL => "https://gate.d5.mpi-inf.mpg.de/aida/service/disambiguate",
 
-	//This one actually works:
-//	CURLOPT_URL => "http://www.htmlcodetutorial.com/cgi-bin/mycgi.pl",
-	CURLOPT_RETURNTRANSFER => 1,
-	CURLOPT_POST => 1, 
-	CURLOPT_POSTFIELDS =>
-		"text=Dylan was born in Duluth"
-	)
-);
+require_once 'alchemyapi.php';
+$alchemyapi = new AlchemyAPI();
 
-$a = curl_exec($curl);
-echo $a;
-curl_close($curl);
+//$demo_text = "Dylan is born in Duluth.";
+$demo_text = $_POST['text'];
+
+
+$response = $alchemyapi->concepts('text',$demo_text, null);
+
+if ($response['status'] == 'OK') {
+	//echo '## Response Object ##', '<br>';
+	//echo print_r($response);
+
+	//echo '<br>';
+	//echo '## Concepts ##', '<br>';
+	foreach ($response['concepts'] as $concept) {
+		//echo 'concept: ', $concept['text'], '<br>';
+		//echo 'relevance: ', $concept['relevance'], '<br>';
+		//echo 'dbpedia: ', $concept['dbpedia'], '<br>';
+		//echo '<br>';
+
+		//DBPedia
+		if(array_key_exists('dbpedia',$concept)){
+			$requestUrl = getUrlDbpediaAbstractURL($concept['dbpedia']);
+			$resultArray = request($requestUrl);
+			$resultArray = json_decode($resultArray,true);
+			if(!empty($resultArray['results']["bindings"])){
+				echo $resultArray['results']["bindings"][0]["abstract"]["value"];
+			}
+		}
+	}
+} else {
+	echo 'Error in the concept tagging call: ', $response['statusInfo'];
+}
+
+
+function getUrlDbpediaAbstract($term){
+   $format = 'json';
+
+   $query = 
+   "PREFIX dbp: <http://dbpedia.org/resource/>
+   PREFIX dbp2: <http://dbpedia.org/ontology/>
+ 
+   SELECT ?abstract
+   WHERE {
+      dbp:".$term." dbp2:abstract ?abstract . 
+      FILTER langMatches(lang(?abstract), 'en')
+   }";
+   
+   $searchUrl = 'http://dbpedia.org/sparql?'
+      .'query='.urlencode($query)
+      .'&format='.$format;
+
+   return $searchUrl;
+}
+
+function getUrlDbpediaAbstractURL($url){
+   $format = 'json';
+
+   $query = 
+   "PREFIX dbp: <http://dbpedia.org/resource/>
+   PREFIX dbp2: <http://dbpedia.org/ontology/>
+ 
+   SELECT ?abstract
+   WHERE {
+      <".$url."> dbp2:abstract ?abstract . 
+      FILTER langMatches(lang(?abstract), 'en')
+   }";
+   
+   $searchUrl = 'http://dbpedia.org/sparql?'
+      .'query='.urlencode($query)
+      .'&format='.$format;
+
+   return $searchUrl;
+}
+
+function request($url){
+ 
+   // is curl installed?
+   if (!function_exists('curl_init')){ 
+      die('CURL is not installed!');
+   }
+   
+   // get curl handle
+   $ch= curl_init();
+
+   // set request url
+   curl_setopt($ch, 
+      CURLOPT_URL, 
+      $url);
+
+   // return response, don't print/echo
+   curl_setopt($ch, 
+      CURLOPT_RETURNTRANSFER, 
+      true);
+ 
+   $response = curl_exec($ch);
+   
+   curl_close($ch);
+   
+   return $response;
+}
+
+function printArray($array, $spaces = ""){
+   $retValue = "";
+   
+   if(is_array($array))
+   {  
+      $spaces = $spaces
+         ."&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;";
+
+      $retValue = $retValue."<br/>";
+
+      foreach(array_keys($array) as $key)
+      {
+         $retValue = $retValue.$spaces
+            ."<strong>".$key."</strong>"
+            .printArray($array[$key], 
+               $spaces);
+      }     
+      $spaces = substr($spaces, 0, -30);
+   }
+   else $retValue = 
+      $retValue." - ".$array."<br/>";
+   
+   return $retValue;
+}
 ?>
